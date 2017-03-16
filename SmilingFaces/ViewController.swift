d8ac3cb50d12e8f49fd581f0dd1e5d8c781d5e69
Jason <jawong1212@gmail.com>
2017-03-15 22:04:55 -0700

    lab 7

---
+++ SmilingFaces/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  SmilingFaces
+//
+//  Created by Lum Situ on 3/15/17.
+//  Copyright © 2017 Lum Situ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var trayView: UIView!
+    
+    var newlyCreatedFace: UIImageView!
+    var newLyCreatedCenter: CGPoint!
+    var originalCenter: CGPoint!
+    var traycenterOpen: CGPoint!
+    var traycenterClose: CGPoint!
+    var newPosition : CGPoint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        traycenterOpen = CGPoint(x:trayView.center.x,y:trayView.center.y)
+        traycenterClose = CGPoint(x:trayView.center.x,y:trayView.center.y + 260)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+
+    @IBAction func onTrayPanGesture(_ sender: UIPanGestureRecognizer) {
+        // Absolute (x,y) coordinates in parentView
+        let location = sender.location(in: view)
+        //let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: self.trayView)
+        
+        if velocity.y > 0 {
+            UIView.animate(withDuration: 0.3 , animations: {
+                self.trayView.center = self.traycenterClose
+            })
+            trayView.center = traycenterClose
+        } else {
+            UIView.animate(withDuration: 0.3 , animations: {
+                    self.trayView.center = self.traycenterOpen
+            })
+            
+        }
+        if (sender.state == UIGestureRecognizerState.began) {
+            NSLog("Gesture began at: %@", NSStringFromCGPoint(location));
+            
+            originalCenter = trayView.center
+        } else if (sender.state == UIGestureRecognizerState.changed) {
+            NSLog("Gesture changed at: %@", NSStringFromCGPoint(location));
+            
+            trayView.center = CGPoint(x:originalCenter.x, y:originalCenter.y + sender.translation(in: self.trayView).y)
+            
+           
+            
+            
+        } else if (sender.state == UIGestureRecognizerState.ended) {
+            NSLog("Gesture ended at: %@", NSStringFromCGPoint(location));
+        }
+    }
+    
+    @IBAction func movemoji(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        let uimgview = sender.view as! UIImageView
+        
+        
+        
+        
+//        let faceCenter = CGPoint(x:self.newlyCreatedFace.center.x, y:self.newlyCreatedFace.center.y)
+        
+//        self.newlyCreatedFace.center = CGPoint(x: faceCenter.x, y: faceCenter.y + trayView.frame.origin.y)
+        if (sender.state == UIGestureRecognizerState.began) {
+            NSLog("Gesture began at: %@", NSStringFromCGPoint(translation));
+            self.newlyCreatedFace = UIImageView.init(image: uimgview.image)
+            self.view.addSubview(self.newlyCreatedFace)
+            
+            self.newlyCreatedFace.center = uimgview.center
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newLyCreatedCenter = newlyCreatedFace.center
+        } else if (sender.state == UIGestureRecognizerState.changed) {
+            NSLog("Gesture changed at: %@", NSStringFromCGPoint(translation));
+            self.newlyCreatedFace.center = CGPoint(x:newLyCreatedCenter.x + translation.x, y: newLyCreatedCenter.y + translation.y)
+            
+        } else if (sender.state == UIGestureRecognizerState.ended) {
+            NSLog("Gesture ended at: %@", NSStringFromCGPoint(translation));
+            
+            
+        }
+        
+        
+    }
+    
+
+}
+
